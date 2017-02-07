624344f5f1be7122b2bc82148d51dd38c882a741
Giuseppe Carlino <beps@Giuseppes-MBP.local>
2017-02-07 16:24:27 +0100

    Code cleanup.

--- arinfopy.py
+++ arinfopy.py
@@ -3,7 +3,8 @@
 ###############################################################################
 #
 # arinfopy parser for ADSO/bin files.
-# Copyright (C) 2013 by Giuseppe Carlino (Simularia s.r.l.) g.carlino@simularia.it
+# Copyright (C) 2013 by Giuseppe Carlino (Simularia s.r.l.)
+#                       g.carlino@simularia.it
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,7 +18,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
 #
 #
 # Simularia S.r.l.
@@ -27,7 +29,6 @@
 # info@simularia.it
 #
 ###############################################################################
-
 
 import sys
 import os
@@ -45,7 +46,6 @@ size = {'int': 4,
 
 class adsobin(object):
     '''Class to read data from ADSO/BIN file.'''
-
 
     def __init__(self, filename):
         '''
@@ -66,16 +66,16 @@ class adsobin(object):
                 'rec2': self.size['rec1'],
                 'rec3': (self.size['rec1'] + self.size['rec2']),
                 'rec4': (self.size['rec1'] + self.size['rec2'] +
-                    self.size['rec3']),
+                         self.size['rec3']),
                 'rec5': (self.size['rec1'] + self.size['rec2'] +
-                    self.size['rec3'] + self.size['rec4']),
+                         self.size['rec3'] + self.size['rec4']),
                 'rec6': (self.size['rec1'] + self.size['rec2'] +
-                    self.size['rec3'] + self.size['rec4'] + self.size['rec5']),
+                         self.size['rec3'] + self.size['rec4'] +
+                         self.size['rec5']),
                 'rec7': (self.size['rec1'] + self.size['rec2'] +
-                    self.size['rec3'] + self.size['rec4'] + self.size['rec5'] +
-                    self.size['rec6'])
+                         self.size['rec3'] + self.size['rec4'] +
+                         self.size['rec5'] + self.size['rec6'])
                 }
-
 
     def getRecord1(self, deadline):
         '''
@@ -86,10 +86,9 @@ class adsobin(object):
         '''
         start = (deadline - 1) * self.size['blockSize'] + self.offset['rec1']
         start, binData = self.__readADSOChunk(start, self.__data)
-        _ident1 = struct.unpack('@8s',binData)[0].decode("utf-8")
+        _ident1 = struct.unpack('@8s', binData)[0].decode("utf-8")
         # logger.debug('ident1 : {}'.format(_ident1))
         return _ident1
-
 
     def getVersion(self):
         '''
@@ -103,7 +102,6 @@ class adsobin(object):
             version = header[-3:]
         return version
 
-
     def getRecord2(self, deadline):
         '''
         Returns file generator
@@ -113,12 +111,11 @@ class adsobin(object):
         # logger.debug('--- Read Record 2 ---')
         start = (deadline - 1) * self.size['blockSize'] + self.offset['rec2']
         start, binData = self.__readADSOChunk(start, self.__data)
-        _ident2 = struct.unpack('@8s',binData)[0].decode("utf-8")
+        _ident2 = struct.unpack('@8s', binData)[0].decode("utf-8")
         # logger.debug('ident2 : {}'.format(_ident2))
         return _ident2
 
-
-    def getRecord3(self, deadline = 1, offset = None):
+    def getRecord3(self, deadline=1, offset=None):
         '''
         Read record 3 of deadline.
 
@@ -142,7 +139,8 @@ class adsobin(object):
                3 integers ignored
         '''
         # logger.debug('--- Read Record 3 ---')
-        if offset != None:
+        #  if offset != None:
+        if offset is not None:
             start = offset
         else:
             start = (deadline - 1) * self.size['blockSize'] + \
@@ -150,20 +148,20 @@ class adsobin(object):
         __nStart, __binData = self.__readADSOChunk(start, self.__data)
         __num = struct.unpack('@27i', __binData)
         __rec3 = {'ijozer': __num[0], 'imozer': __num[1], 'ianzer': __num[2],
-                'ihezer': __num[3], 'imizer': __num[4], 'isezer': __num[5],
-                'ijozei': __num[6], 'imozei': __num[7], 'ianzei': __num[8],
-                'ihezei': __num[9], 'imizei': __num[10], 'isezei': __num[11],
-                'immai': __num[12], 'jmmai': __num[13], 'kmmai': __num[14],
-                'nreper': __num[15], 'nvar3d': __num[16], 'nvar2d': __num[17],
-                'nevt': __num[18], 'itmax': __num[19], 'nevtpr': __num[20],
-                'itmopro': __num[21], 'IINDEX': __num[22], 'IKSURF': __num[23]}
+                  'ihezer': __num[3], 'imizer': __num[4], 'isezer': __num[5],
+                  'ijozei': __num[6], 'imozei': __num[7], 'ianzei': __num[8],
+                  'ihezei': __num[9], 'imizei': __num[10], 'isezei': __num[11],
+                  'immai': __num[12], 'jmmai': __num[13], 'kmmai': __num[14],
+                  'nreper': __num[15], 'nvar3d': __num[16],
+                  'nvar2d': __num[17], 'nevt': __num[18], 'itmax': __num[19],
+                  'nevtpr': __num[20], 'itmopro': __num[21],
+                  'IINDEX': __num[22], 'IKSURF': __num[23]}
         if __rec3['ianzer'] < 1000:
             __rec3['ianzer'] += 2000
         if __rec3['ianzei'] < 1000:
             __rec3['ianzei'] += 2000
 
         return __rec3
-
 
     def getRecord4(self, deadline):
         '''
@@ -212,7 +210,6 @@ class adsobin(object):
                 'ztop': ztop}
         return rec4
 
-
     def getRecord5(self, deadline):
         '''
         Read record 5 of deadline
@@ -248,20 +245,24 @@ class adsobin(object):
         nomvar2d = ''
         univar2d = ''
 
-        creper = [struct.unpack('@8s', binData[i:rec3['nreper']*8])[0] for i in
-                range(rec3['nreper'])]
+        creper = [struct.unpack('@8s', binData[i:rec3['nreper']*8])[0]
+                  for i in range(rec3['nreper'])]
         offset = (rec3['nreper']) * 8
         nomvar3d = [struct.unpack('@8s', binData[offset+i*8:offset +
-            (i+1)*8])[0].decode('utf-8') for i in range(rec3['nvar3d'])]
+                    (i+1)*8])[0].decode('utf-8') for i in
+                    range(rec3['nvar3d'])]
         offset = (rec3['nreper'] + rec3['nvar3d']) * 8
         univar3d = [struct.unpack('@8s', binData[offset+i*8:offset +
-            (i+1)*8])[0].decode('utf-8') for i in range(rec3['nvar3d'])]
+                    (i+1)*8])[0].decode('utf-8') for i in
+                    range(rec3['nvar3d'])]
         offset = (rec3['nreper'] + 2*rec3['nvar3d']) * 8
         nomvar2d = [struct.unpack('@8s', binData[offset+i*8:offset +
-            (i+1)*8])[0].decode('utf-8') for i in range(rec3['nvar2d'])]
+                    (i+1)*8])[0].decode('utf-8') for i in
+                    range(rec3['nvar2d'])]
         offset = (rec3['nreper'] + 2*rec3['nvar3d'] + rec3['nvar2d']) * 8
-        univar2d = [struct.unpack('@8s', binData[offset+i*8:offset
-            +(i+1)*8])[0].decode('utf-8') for i in range(rec3['nvar2d'])]
+        univar2d = [struct.unpack('@8s', binData[offset+i*8:offset +
+                    (i+1)*8])[0].decode('utf-8') for i in
+                    range(rec3['nvar2d'])]
 
         rec5 = {'nomvar3d': nomvar3d,
                 'univar3d': univar3d,
@@ -269,7 +270,6 @@ class adsobin(object):
                 'univar2d': univar2d}
         # logger.debug('rec5: {}'.format(rec5))
         return rec5
-
 
     def getRecord6(self, start):
         '''
@@ -281,7 +281,6 @@ class adsobin(object):
         '''
         # logger.debug('--- Read Record 6 ---')
         pass
-
 
     def getRecord7(self, deadline):
         '''
@@ -314,7 +313,6 @@ class adsobin(object):
 
         return rec7
 
-
     def __len__(self):
         '''
         Get number of deadlines.
@@ -328,14 +326,13 @@ class adsobin(object):
         nDeadlines = int(len(self.__data) / self.size['blockSize'])
         return nDeadlines
 
-
     def getDeadlineBlockSize(self):
         '''
         Returns dictionary with the size of each record in bytes
         and the size of the whole deadline in bytes.
         '''
         # Read record 3 of 1st deadline
-        rec3 = self.getRecord3(offset = 32)
+        rec3 = self.getRecord3(offset=32)
 
         # Compute size of each block
         nRec1 = size['char8'] + size['pad']
@@ -351,13 +348,13 @@ class adsobin(object):
             nRec6 = 3 * rec3['nreper'] * size['real'] + size['pad']
         else:
             nRec6 = 0
-        nRec7 = (rec3['nvar3d'] * (size['pad'] + rec3['immai'] * rec3['jmmai']
-            * rec3['kmmai'] * size['real']) +
-            rec3['nvar2d'] * (size['pad'] + rec3['immai'] * rec3['jmmai'] *
-                size['real']))
+        nRec7 = (rec3['nvar3d'] * (size['pad'] + rec3['immai'] *
+                 rec3['jmmai'] * rec3['kmmai'] * size['real']) +
+                 rec3['nvar2d'] * (size['pad'] + rec3['immai'] *
+                 rec3['jmmai'] * size['real']))
 
         nBytesDeadline = (nRec1 + nRec2 + nRec3 + nRec4 + nRec5 + nRec6 +
-                nRec7)
+                          nRec7)
         deadlineBlock = {'rec1': nRec1,
                          'rec2': nRec2,
                          'rec3': nRec3,
@@ -367,7 +364,6 @@ class adsobin(object):
                          'rec7': nRec7,
                          'blockSize': nBytesDeadline}
         return deadlineBlock
-
 
     def __readADSOChunk(self, rStart, rData):
         """
@@ -395,7 +391,6 @@ class adsobin(object):
         # logger.debug('Final offset: {}'.format(rEnd))
         return [rEnd, rBinData]
 
-
     def deadlines(self):
         '''
         Print out list of deadlines in ADSO/BIN file.
@@ -404,13 +399,13 @@ class adsobin(object):
         for nd in range(len(self)):
             rec3 = self.getRecord3(nd + 1)
             dtdeadline = datetime(rec3['ianzer'],
-                    rec3['imozer'],
-                    rec3['ijozer'],
-                    rec3['ihezer'] % 24,
-                    rec3['imizer'],
-                    rec3['isezer'])
+                                  rec3['imozer'],
+                                  rec3['ijozer'],
+                                  rec3['ihezer'] % 24,
+                                  rec3['imizer'],
+                                  rec3['isezer'])
             if rec3['ihezer'] == 24:
-                dtdeadline = dtdeadline + timedelta(days = 1)
+                dtdeadline = dtdeadline + timedelta(days=1)
             print('{} {:>3d} {}'.format(
                 os.path.basename(self.filename),
                 nd + 1,
@@ -424,17 +419,15 @@ class adsobin(object):
         for nd in range(len(self)):
             rec3 = self.getRecord3(nd + 1)
             dtdeadline = datetime(rec3['ianzer'],
-                    rec3['imozer'],
-                    rec3['ijozer'],
-                    rec3['ihezer'] % 24,
-                    rec3['imizer'],
-                    rec3['isezer'])
+                                  rec3['imozer'],
+                                  rec3['ijozer'],
+                                  rec3['ihezer'] % 24,
+                                  rec3['imizer'],
+                                  rec3['isezer'])
             if rec3['ihezer'] == 24:
-                dtdeadline = dtdeadline + timedelta(days = 1)
+                dtdeadline = dtdeadline + timedelta(days=1)
             __deadlines.append(dtdeadline)
         return __deadlines
-
-
 
     def minmax(self):
         '''
@@ -446,34 +439,33 @@ class adsobin(object):
             rec5 = self.getRecord5(nd + 1)
             rec7 = self.getRecord7(nd + 1)
             dtdeadline = datetime(rec3['ianzer'],
-                    rec3['imozer'],
-                    rec3['ijozer'],
-                    rec3['ihezer'] % 24,
-                    rec3['imizer'],
-                    rec3['isezer'])
+                                  rec3['imozer'],
+                                  rec3['ijozer'],
+                                  rec3['ihezer'] % 24,
+                                  rec3['imizer'],
+                                  rec3['isezer'])
             if rec3['ihezer'] == 24:
-                dtdeadline = dtdeadline + timedelta(days = 1)
+                dtdeadline = dtdeadline + timedelta(days=1)
             print('-' * 70)
             print('Fields read at deadline # {:>3d}: {}'.format(nd + 1,
-                dtdeadline.strftime('%d/%m/%Y %H:%M:%S')))
+                  dtdeadline.strftime('%d/%m/%Y %H:%M:%S')))
             print('-' * 70)
             for n3d, name in enumerate(rec5['nomvar3d']):
                 print(('3D # {:>3d}: {:>10s}  min = {:12.4f} max = {:12.4f} ' +
-                        ' [{:s}])').
-                        format(n3d + 1,
-                            name.strip(),
-                            min(rec7[name]),
-                            max(rec7[name]),
-                            rec5['univar3d'][n3d].strip()))
+                      ' [{:s}])').
+                      format(n3d + 1,
+                             name.strip(),
+                             min(rec7[name]),
+                             max(rec7[name]),
+                             rec5['univar3d'][n3d].strip()))
             for n2d, name in enumerate(rec5['nomvar2d']):
                 print(('2D # {:>3d}: {:>10s}  min = {:12.4f} max = {:12.4f} ' +
-                        ' [{:s}]').
-                        format(n2d + 1,
-                            name.strip(),
-                            min(rec7[name]),
-                            max(rec7[name]),
-                            rec5['univar2d'][n2d].strip()))
-
+                      ' [{:s}]').
+                      format(n2d + 1,
+                             name.strip(),
+                             min(rec7[name]),
+                             max(rec7[name]),
+                             rec5['univar2d'][n2d].strip()))
 
     def summary(self):
         '''
@@ -485,21 +477,21 @@ class adsobin(object):
         rec5 = self.getRecord5(len(self))
 
         firstdl = datetime(rec3['ianzei'],
-                rec3['imozei'],
-                rec3['ijozei'],
-                rec3['ihezei'] % 24,
-                rec3['imizei'],
-                rec3['isezei'])
+                           rec3['imozei'],
+                           rec3['ijozei'],
+                           rec3['ihezei'] % 24,
+                           rec3['imizei'],
+                           rec3['isezei'])
         if rec3['ihezei'] == 24:
-            firstdl = firstdl + timedelta(days = 1)
+            firstdl = firstdl + timedelta(days=1)
         lastdl = datetime(rec3['ianzer'],
-                rec3['imozer'],
-                rec3['ijozer'],
-                rec3['ihezer'] % 24,
-                rec3['imizer'],
-                rec3['isezer'])
+                          rec3['imozer'],
+                          rec3['ijozer'],
+                          rec3['ihezer'] % 24,
+                          rec3['imizer'],
+                          rec3['isezer'])
         if rec3['ihezer'] == 24:
-            lastdl = lastdl + timedelta(days = 1)
+            lastdl = lastdl + timedelta(days=1)
         dtsecs = (lastdl - firstdl).total_seconds() / len(self)
 
         print('\n--- ADSO/bin file info ---')
@@ -521,30 +513,29 @@ class adsobin(object):
             rec4['xlatso'], rec4['ylatso']))
         print('Top of the domain           : {:.3f}'.format(rec4['ztop']))
         print('Levels                      : ' + ('{:.2f} ' *
-            len(rec4['sgrid'])).format(*rec4['sgrid']))
+              len(rec4['sgrid'])).format(*rec4['sgrid']))
         print('nvar2d, nvar3d              : {:d}   {:d}'.format(
-            rec3['nvar2d'], rec3['nvar3d']))
-        if rec3['nvar2d'] > 0 :
+              rec3['nvar2d'], rec3['nvar3d']))
+        if rec3['nvar2d'] > 0:
             print('2D variabels                : ' + ('{:<s} ' *
-                rec3['nvar2d']).format(*rec5['nomvar2d']))
-        if rec3['nvar3d'] > 0 :
+                  rec3['nvar2d']).format(*rec5['nomvar2d']))
+        if rec3['nvar3d'] > 0:
             print('3D variables                : ' + ('{:<s} ' *
-                rec3['nvar3d']).format(*rec5['nomvar3d']))
+                  rec3['nvar3d']).format(*rec5['nomvar3d']))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description =
-            'arinfopy parser for ADSO/bin files.')
-    parser.add_argument('inifile', help = 'File to be parsed')
+    parser = argparse.ArgumentParser(description='arinfopy parser for '
+                                     'ADSO/bin files.')
+    parser.add_argument('inifile', help='File to be parsed')
     parser.add_argument('-minmax',
-            help = "Show min/max values for each deadline",
-            action = "store_true")
+                        help="Show min/max values for each deadline",
+                        action="store_true")
     parser.add_argument('-deadlines',
-            help = "Show deadlines",
-            action = "store_true")
+                        help="Show deadlines", action="store_true")
     parser.add_argument('-v', '--verbose',
-            help = 'Increse output verbosity.',
-            action="store_true")
+                        help='Increse output verbosity.',
+                        action="store_true")
     args = parser.parse_args()
 
     # Create Logger
@@ -559,11 +550,11 @@ if __name__ == '__main__':
     console.setLevel(logging.DEBUG)
     console.setFormatter(formatter)
     logger.addHandler(console)
-   # flogname = 'elisestat_' + datetime.now().isoformat() + '.log'
-   # flog = logging.FileHandler(flogname)
-   # flog.setLevel(logging.INFO)
-   # flog.setFormatter(formatter)
-   # logger.addHandler(flog)
+    #  flogname = 'elisestat_' + datetime.now().isoformat() + '.log'
+    #  flog = logging.FileHandler(flogname)
+    #  flog.setLevel(logging.INFO)
+    #  flog.setFormatter(formatter)
+    #  logger.addHandler(flog)
 
     mData = adsobin(args.inifile)
     if args.deadlines:

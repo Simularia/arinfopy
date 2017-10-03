f74711aa7fe5c5b9ea27ef6c0ea0caca59e33aa8
Giuseppe Carlino <g.carlino@simularia.it>
2017-10-03 12:07:43 +0200

    Updated seutp information.

--- setup.py
+++ setup.py
@@ -1,29 +1,11 @@
-import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-# def read(fname):
-#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "arinfopy",
     version = "2.1",
     author = "Giuseppe Carlino",
     author_email = "g.carlino@simularia.it",
-    description = ("A module to read ADSO/BIN information. "
-                                   "It also works as a library."),
-    # license = "BSD",
-    # keywords = "example documentation tutorial",
-    # url = "http://packages.python.org/an_example_pypi_project",
-    # packages=['arinfopy', 'tests'],
+    description = ("A module to read ADSO/BIN information."),
+    license = "GNU GPLv2",
     packages=['.']
-    # long_description=read('README.md')
-    # classifiers=[
-        # "Development Status :: 3 - Alpha",
-        # "Topic :: Utilities",
-        # "License :: OSI Approved :: BSD License",
-    # ],
 )
